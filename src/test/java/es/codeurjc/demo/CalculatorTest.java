8a96ac991dc09812814625427a7b9e6a05be69b8
raultejada24 <raulintm@gmail.com>
2026-04-13 20:58:40 +0200

    fix: prevent calculator from accepting negative numbers

--- src/test/java/es/codeurjc/demo/CalculatorTest.java
+++ src/test/java/es/codeurjc/demo/CalculatorTest.java
@@ -5,4 +5,13 @@ public class CalculatorTest extends TestCase {
         Calculator calc = new Calculator();
         assertEquals(5, calc.add(2, 3));
     }
+    public void testAddNegative() {
+        Calculator calc = new Calculator();
+        try {
+            calc.add(-1, 5);
+            fail("Expected an exception");
+        } catch (IllegalArgumentException e) {
+            assertEquals("No negative numbers allowed!", e.getMessage());
+        }
+    }
 }
